844054164099dbb764d4803a4a6eab4f127380ae
Kael Zhang <i+github@kael.me>
2019-04-02 18:28:28 +0800

    WIP

---
+++ src/error.js
@@ -0,0 +1,8 @@
+const {Errors} = require('err-object')
+const {E, error} = new Errors()
+
+E()
+
+module.exports = {
+  error
+}
---
+++ src/helper.js
@@ -0,0 +1,74 @@
+const {stringify} = require('querystring')
+
+const makeNamespaceArray = n => Array.isArray(n)
+  ? n.length === 0
+    ? ['application']
+    : n
+  : [n]
+
+const isPlainObject = o => Object.keys(o).length === 0
+
+const createQuery = options => {
+  const query = Object.keys(options).reduce((ret, key) => {
+    const v = options[key]
+    if (v !== undefined && v !== null) {
+      ret[key] = v
+    }
+
+    return ret
+  }, {})
+
+  if (isPlainObject(query)) {
+    return ''
+  }
+
+  return `?${stringify(query)}`
+}
+
+const queryConfig = (
+  // host
+  h,
+  // appId
+  a,
+  // cluster
+  c,
+  // namespace
+  n,
+  // releaseKey: optional
+  r,
+  // ip: optional
+  i,
+  // dataCenter: optional
+  d,
+  // messages: optional
+  m
+
+) => `${h}/configs/${a}/${c}/${n}${
+  createQuery({
+    releaseKey: r,
+    ip: i,
+    dataCenter: d,
+    messages: m
+  })
+}`
+
+const queryConfigAsJson = (
+  h,
+  a,
+  c,
+  n,
+  // ip: optional
+  i,
+  // dataCenter: optional
+  d
+) => `${h}/configfiles/json/${a}/${c}/${n}${
+  createQuery({
+    ip: i,
+    dataCenter: d
+  })
+}`
+
+module.exports = {
+  queryConfigAsJson,
+  queryConfig
+}
